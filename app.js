7ba3d5e2aaaabc02fd9a2b4c5168145b3f483efe
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-06 11:13:06 +0300

    background-restart commit

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const enter = document.querySelector('#enter');
 const restart = document.querySelector('#restart');
 const output = document.querySelector(".outputext");
 const random = Math.floor(Math.random() * 100 + 1);
-let life = 5;
+let life = 20;
 
 
 
 const guessGame=function(){
-    if(life>0){
+    if(life>1){
     let guess = document.querySelector('#input').value;
     if(guess < 0 || guess > 100){
         output.innerHTML = "Please enter a number between 1 and 100";
     }else{
         if(guess == random){
-            output.innerHTML = "You guessed the number!";
+            output.innerHTML = `You guessed the number ${guess}!`;
+            document.querySelector("body").style.backgroundColor = "green";
+            
         }else if(guess > random){
             output.innerHTML = "Your guess is too high";
             life--;
@@ -21,12 +23,18 @@ const guessGame=function(){
             output.innerHTML = "Your guess is too low";
             life--;
         }
+      
+
 
     }
 
     
 }else{
     output.innerHTML = "You have no more hearts";
+    enter.disabled = true;
+    restart.style.display = "inline-block";
+    document.querySelector("body").style.backgroundColor = "red";
+    
     
 }
 }
