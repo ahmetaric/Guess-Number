017e34554de79cf068b39d4f2e42acb859c74f1c
ahmetaric <ahmet.aric1965@gmail.com>
2022-06-06 14:55:33 +0300

    heart-between commit

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const enter = document.querySelector('#enter');
 const restart = document.querySelector('#restart');
 const output = document.querySelector(".outputext");
+const start = document.querySelector(".start");
+const end = document.querySelector(".end");
+const heart = document.querySelector(".heart");
+const heartNumber = document.querySelector(".heart-number");
 const random = Math.floor(Math.random() * 100 + 1);
 let life = 20;
 
@@ -15,13 +19,20 @@ const guessGame=function(){
         if(guess == random){
             output.innerHTML = `You guessed the number ${guess}!`;
             document.querySelector("body").style.backgroundColor = "green";
+            restart.style.display = "inline-block";
+            enter.disabled = true;
+            heart.innerText = "Congratulations! You won!";
             
         }else if(guess > random){
             output.innerHTML = "Your guess is too high";
+            end.innerText = guess;
             life--;
+            heartNumber.innerText = life;
         }else if(guess < random){
             output.innerHTML = "Your guess is too low";
+            start.innerText = guess;
             life--;
+            heartNumber.innerText = life;
         }
       
 
@@ -43,3 +54,14 @@ const guessGame=function(){
 
 
 enter.addEventListener("click",guessGame);
+input.addEventListener("keydown",function(e){
+    if(e.keyCode == 13){
+        guessGame();
+    }
+})
+
+// input.addEventListener("keydown",function(e){
+//     if(e.keyCode == 3){
+//        window.location.reload();
+//     }
+// })
